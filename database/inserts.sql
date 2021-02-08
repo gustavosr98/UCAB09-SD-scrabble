5a5533d8619b66e950d57c5e91fae6927ec45d90
Javier Andrade <javierandrade2107@gmail.com>
2021-02-08 19:23:35 -0400

    Scripts inserts database y ruta back

---
+++ database/inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO status (name, description) VALUES ('Created', 'The game was created. Waiting for players.');
+INSERT INTO status (name, description) VALUES ('InProgress', 'The game is in progress.');
+INSERT INTO status (name, description) VALUES ('Finished', 'The game finished.');
