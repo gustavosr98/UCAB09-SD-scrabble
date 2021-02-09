98af8e0da3c220d619041ff4ae972063d06b5197
Gustavo Sanchez <gustavosr98@gmail.com>
2021-02-09 01:43:44 -0400

    03/CLIENT-ZK: Basics done

--- client/src/views/Login/Login.vue
+++ client/src/views/Login/Login.vue
@@ -2,15 +2,7 @@
   <v-container class="container" fluid fill-height>
     <v-row class="justify-center">
       <v-col>
-        <v-text-field outlined placeholder="key" v-model="key"></v-text-field>
-        <v-text-field
-          outlined
-          placeholder="value"
-          v-model="value"
-        ></v-text-field>
-        <v-btn @click="create">create node</v-btn>
-        <v-btn @click="setData">set value</v-btn>
-        <v-btn @click="getData">get value</v-btn>
+        Login
       </v-col>
     </v-row>
   </v-container>
@@ -18,25 +10,9 @@
 
 <script>
 import routes from "@/router/routes";
-import { zkClient } from "@/connections/zookeeper/zk-client";
 
 export default {
   name: "login",
-  data() {
-    return { key: null, value: null, zkClient };
-  },
-  mounted() {},
-  methods: {
-    create() {
-      zkClient.create(this.key);
-    },
-    setData() {
-      zkClient.setData(this.key, { value: this.value });
-    },
-    getData() {
-      console.log(zkClient.getData(this.key));
-    },
-  },
 };
 </script>
 
---
+++ client/src/views/Test/Zookeeper.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="container" fluid fill-height>
+    <v-row class="justify-center">
+      <v-col>
+        <v-text-field outlined placeholder="key" v-model="key"></v-text-field>
+        <v-text-field
+          outlined
+          placeholder="value"
+          v-model="value"
+        ></v-text-field>
+        <v-btn @click="create">create node</v-btn>
+        <v-btn @click="setData">set value</v-btn>
+        <v-btn @click="getData">get value</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import routes from "@/router/routes";
+import zkClient from "@/connections/zookeeper/zk-client";
+
+export default {
+  name: "login",
+  data() {
+    return { key: null, value: null };
+  },
+  mounted() {},
+  methods: {
+    async create() {
+      await zkClient.create(this.key);
+    },
+    setData() {
+      zkClient.setData(this.key, this.value);
+    },
+    async getData() {
+      console.log(await zkClient.getData(this.key));
+    },
+  },
+};
+</script>
+
+<style scoped></style>
