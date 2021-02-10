bda7aa4fdd0654f2b90e060e4b3ef45a4f8219e3
Gustavo Sanchez <gustavosr98@gmail.com>
2021-02-10 14:04:24 -0400

    11/MULTIPLAYER: Windows size fix

--- client/src/background.js
+++ client/src/background.js
@@ -13,8 +13,10 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 720,
+    height: 720,
+    minWidth: 1080,
+    minHeight: 720,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
